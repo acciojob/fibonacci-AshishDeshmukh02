e83b51d2a6fc2ec59235d7077204f3f477357f9e
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-02-22 21:11:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
-// your code here
+  // base cases
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  }
+  // recursive case
+  else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
 }
+
 
 module.exports = fibonacci;
